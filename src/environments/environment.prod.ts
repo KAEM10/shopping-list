18337630c339f970eee9731e51f6828b85f62411
doncruz <cruzcamilo129@gmail.com>
2024-12-10 18:23:40 -0500

    Se agregó la variable de inicialización de firebase al ambiente de producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCWoFzSiLl0l1rgONdyqrdXlxjKoSLbYoM",
+    authDomain: "shopping-list-unicauca.firebaseapp.com",
+    projectId: "shopping-list-unicauca",
+    storageBucket: "shopping-list-unicauca.firebasestorage.app",
+    messagingSenderId: "759528023549",
+    appId: "1:759528023549:web:20554465c622338556a594",
+    measurementId: "G-W0DQZPY7ER"
+  }
 };
